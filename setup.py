39f5ece27acee4057b1f3cc21bb06cac1d48efac
Swift <theycallmeswift@gmail.com>
2013-03-13 12:52:06 -0700

    Version bump (0.0.3)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "BreakfastSerial",
-    version = "0.0.2",
+    version = "0.0.3",
     description = "Python Framework for interacting with Arduino",
     author = "Swift",
     author_email = "theycallmeswift@gmail.com",
