e275925af6d52c330922ea12223869e1791477e7
Swift <theycallmeswift@gmail.com>
2013-03-13 00:09:27 -0500

    Added light switch example.

---
+++ examples/light_switch.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+"""
+This is an example that demonstrates how to use a
+button to control an led with breakfast_serial. It
+assumes you have an button wired up to pin 8 and an
+led wired to pin 13.
+"""
+from breakfast_serial import Arduino, Led, Button
+
+board = Arduino()
+button = Button(board, 8)
+led = Led(board, 13)
+
+button.down(led.toggle)
+
+# Run an interactive shell so you can play (not required)
+import code
+code.InteractiveConsole(locals=globals()).interact()
